3cf220a88b91dfbf97422ad480850de26e9f6528
Carmen-Mercado <carmen24597mr@gmail.com>
2022-01-27 15:14:59 -0600

    Add util jets and test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,15 +1,37 @@
-const sum = require('./sum.js');
-const utils = require('./../assets/js/utils')
+const utils = require('./../src/assets/js/utils')
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
-test('validate files', () => {
+test('validate empty all files', () => {
     form ={
         email:null,
         subject:null,
         body:null
     };
     expect(utils(form)).toBe(false);
+});
+
+test('validate empty one file', () => {
+    form ={
+        email:'some@test.com',
+        subject:'test',
+        body:null
+    };
+    expect(utils(form)).toBe(false);
+});
+
+test('validate empty two file', () => {
+    form ={
+        email:'some@test.com',
+        subject:null,
+        body:null
+    };
+    expect(utils(form)).toBe(false);
+});
+
+test('All values', () => {
+    form ={
+        email:'some@test.com',
+        subject:'test',
+        body:"some text"
+    };
+    expect(utils(form)).toBe(true);
 });
